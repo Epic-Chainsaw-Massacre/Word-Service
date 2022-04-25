3330bc2df37893f6b5947837149c31adb52be2d0
CrossyChainsaw <kaangogcay@gmail.com>
2022-04-25 22:34:27 +0200

    Add API Rate limit, doesnt keep running after closing app

--- src/main/java/EpicChainsawMassacre/reversehangmanonlinebackend/resources/WordResource.java
+++ src/main/java/EpicChainsawMassacre/reversehangmanonlinebackend/resources/WordResource.java
@@ -1,14 +1,35 @@
 package EpicChainsawMassacre.reversehangmanonlinebackend.resources;
 
 import EpicChainsawMassacre.reversehangmanonlinebackend.models.Word;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.json.JSONArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/word")
 public class WordResource {
+
+    // this means
+    Refill refillSeconds = Refill.intervally(1, Duration.ofSeconds(5)); // refill back to 1 token every 5 seconds
+    Refill refillHours = Refill.intervally(50, Duration.ofHours(1)); // refill back to 50 tokens every 1 hour
+    Refill refillDays = Refill.intervally(1000, Duration.ofDays(1)); // refill back to 1000 tokens every 1 day
+    Bandwidth limitSeconds = Bandwidth.classic(1, refillSeconds); // every 5 seconds max 1 request
+    Bandwidth limitHours = Bandwidth.classic(50, refillHours); // every 1 hour max 50 requests
+    Bandwidth limitDays = Bandwidth.classic(1000, refillDays); // every 1 day max 1000 requests
+    Bucket bucket = Bucket4j.builder()
+            .addLimit(limitSeconds)
+            .addLimit(limitHours)
+            .addLimit(limitDays)
+            .build();
 
     @RequestMapping("/koeieuier")
     Word koeieuier() {
@@ -23,13 +44,17 @@ public class WordResource {
 
     @RequestMapping("/check")
     @ResponseBody
-    Word checkIfWordExists(@RequestParam String word) {
+    ResponseEntity<Word> checkIfWordExists(@RequestParam String word) {
         System.out.println("Parameter: " + word);
-        if (wordExists(word)) {
-            return new Word(word, true);
+        if (bucket.tryConsume(1)) {
+            if (wordExists(word)) {
+                return ResponseEntity.ok(new Word(word, true));
+            } else {
+                return ResponseEntity.ok(new Word(word, false));
+            }
         }
         else {
-            return new Word(word, false);
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
         }
     }
 
@@ -53,6 +78,7 @@ public class WordResource {
             return wordString.contains(word);
     }
 
+    //1000 per dag
     //https://dictionaryapi.com/api/v3/references/sd2/json/PUT_WORD_HERE?key=4084046c-2911-430f-9666-a5ca6a3714b4
 
     @RequestMapping("/cool")
--- src/test/java/EpicChainsawMassacre/reversehangmanonlinebackend/ReverseHangmanOnlineBackendApplicationTests.java
+++ src/test/java/EpicChainsawMassacre/reversehangmanonlinebackend/ReverseHangmanOnlineBackendApplicationTests.java
@@ -1,8 +1,15 @@
 package EpicChainsawMassacre.reversehangmanonlinebackend;
 
 import EpicChainsawMassacre.reversehangmanonlinebackend.resources.WordResource;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -21,4 +28,36 @@ class ReverseHangmanOnlineBackendApplicationTests {
 		assertFalse(wordResource.wordExists("marc"));
 	}
 
+	@Test
+	void bucketTestfake() {
+		Refill refill = Refill.intervally(10, Duration.ofMinutes(1));
+		Bandwidth limit = Bandwidth.classic(10, refill);
+		Bucket bucket = Bucket4j.builder()
+				.addLimit(limit)
+				.build();
+
+		for (int i = 1; i <= 10; i++) {
+			assertTrue(bucket.tryConsume(1));
+		}
+		assertFalse(bucket.tryConsume(1));
+	}
+
+	@Test
+	void apiBucket(){
+		// so every minute you can use 5 tokens
+		Refill refill = Refill.intervally(5, Duration.ofMinutes(1));
+		// there are 5 tokens in total?
+		Bandwidth limit = Bandwidth.classic(5, refill);
+		// Build the bucket
+		Bucket bucket = Bucket4j.builder()
+				.addLimit(limit)
+				.build();
+
+		// consume all 5 tokens
+		for (int i = 1; i <= 5; i++) {
+			assertTrue(bucket.tryConsume(1));
+		}
+		// should be no tokens left
+		assertFalse(bucket.tryConsume(1));
+	}
 }
